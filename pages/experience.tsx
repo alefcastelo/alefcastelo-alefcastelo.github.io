e1d5d7c8c63e967c7103e243e668d809970318e1
Alef Castelo <alefaraujocastelo@gmail.com>
2020-11-14 10:26:19 -0600

    fixing imports

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Layout from '../../components/Layout'
-import * as S from '../../styles/styled'
-import { ExperienceGroup } from '../../types'
-import ExperienceItem from './components/experience-group'
-import data from './data.json'
+import Layout from '../components/layout'
+import * as S from '../styles/styled'
+import { ExperienceGroup } from '../types'
+import ExperienceItem from '../components/experience-group'
+import data from '../data.json'
 
 const Experience: React.FC = () => {
   return (
@@ -12,7 +12,7 @@ const Experience: React.FC = () => {
         <S.MainContent>
           <S.Container>
             <S.SectionTitle>Experience</S.SectionTitle>
-            {data.map((experience: ExperienceGroup, index: number) => (
+            {data.experiencies.map((experience: ExperienceGroup, index: number) => (
               <ExperienceItem key={index} experienceGroup={experience} isVisible={true} />
             ))}
           </S.Container>
