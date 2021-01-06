849e2480058f57ebb48972b3a436b14c9e85bc67
Alef Castelo <alefaraujocastelo@gmail.com>
2021-01-06 17:27:41 +0000

    adding static page generator for markdown

---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,52 @@
+import Layout from '../../components/layout'
+import { getAllPosts, getPostBySlug } from '../../lib/api'
+import markdownToHtml from '../../lib/markdown-to-html'
+
+export default function Doc({  post }) {
+  return (
+      <>
+        <Layout>
+            <h1>{post.title}</h1>
+            <div dangerouslySetInnerHTML={{ __html:  post.content }} />
+        </Layout>
+      </>
+  )
+    
+}
+
+export async function getStaticProps({ params }) {
+    const post: any = getPostBySlug(params.slug, [
+      'title',
+      'date',
+      'slug',
+      'author',
+      'content',
+      'ogImage',
+      'coverImage',
+    ])
+    const content = await markdownToHtml(post.content || '')
+  
+    return {
+      props: {
+        post: {
+          ...post,
+          content,
+        },
+      },
+    }
+  }
+  
+  export async function getStaticPaths() {
+    const posts = getAllPosts(['slug'])
+  
+    return {
+      paths: posts.map((post: any) => {
+        return {
+          params: {
+            slug: post.slug,
+          },
+        }
+      }),
+      fallback: false,
+    }
+  }
